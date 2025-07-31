ae2d6d7e4a6eb7275d05032201c22d520c5bb41d
DarkSevenX <nargumedor@unicartagena.edu.co>
2025-07-31 12:08:02 -0500

    Arregla el alto de el chatbot

--- src/components/Acedro.jsx
+++ src/components/Acedro.jsx
@@ -1,12 +1,23 @@
 import "../styles/bot.css"
+import { useState } from "react"
 
 export const Acedro = () => {
+    const [open, setOpen] = useState(true);
+
+    const handleOverlayClick = () => setOpen(false);
+
     return (
-        <iframe
-        title="Bot"
-        className="robot-ia"
-        src="https://landbot.online/v3/H-3004656-WR56YJX4P3453SFM/index.html"
-        id="bot"
-        frameborder="0"></iframe>
+        <>
+            {open && <div className="chatbot-overlay" onClick={handleOverlayClick}></div>}
+            {open && (
+                <iframe
+                    title="Bot"
+                    className="robot-ia"
+                    src="https://landbot.online/v3/H-3004656-WR56YJX4P3453SFM/index.html"
+                    id="bot"
+                    frameBorder="0"
+                ></iframe>
+            )}
+        </>
     )
 }
