257bb8b7b5fa5a556deee54e7a16e204eaa6bde0
DarkSevenX <nargumedor@unicartagena.edu.co>
2025-08-02 14:29:26 -0500

    refactor: reestructuración de sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,17 +1,27 @@
 import emailjs from "emailjs-com"
 const apiKey = import.meta.env.VITE_API_KEY
 
+// guillo revisa esta vaina porfa
+// le cambié la estructura para que maneje los errores
+// y para que limpie los campos del formulario después de enviar el correo
+// parece que se estaban limpiando antes de confirmar que todo fue exitoso
 export const sendMail = (name, asunto, mail, msg, setName, setAsunto, setMail, setMsg) => {
-    emailjs.send("service_hosal24", "template_9huakp7", {
-        title: asunto,
-        name,
-        message: msg,
-        email: mail,
-    }, apiKey).then((response) => {
-        return response
-    })
-    setName("")
-    setAsunto("")
-    setMail("")
-    setMsg("")
+    try {
+        const response = emailjs.send("service_hosal24", "template_9huakp7", {
+            title: asunto,
+            name,
+            message: msg,
+            email: mail,
+        }, apiKey)
+
+        setName('');
+        setAsunto('');
+        setMail('');
+        setMsg('');
+
+        return response;
+    } catch (error) {
+        console.error("Error enviando el email:", error);
+        throw new Error("Fallo al enviar el email.");   
+    }
 }
